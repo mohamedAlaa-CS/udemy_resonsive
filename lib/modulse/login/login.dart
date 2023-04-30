9e08fad00c04929bb2ac15a8a279de588dad14f6
Myproject <mohamed.alaa@gmail.com>
2023-04-30 16:20:45 -0700

    Edit

--- lib/modulse/login/login.dart
+++ lib/modulse/login/login.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/shared/combonans/combonans.dart';
-//import 'package:flutter/widgets.dart';
+// update
 
 class LoginScrean extends StatelessWidget {
   var emailController = TextEditingController();
